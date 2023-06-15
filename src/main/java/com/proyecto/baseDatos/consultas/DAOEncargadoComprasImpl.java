4e02934cf4385b06764bc036bdf3d074da6144af
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-06-14 22:28:26 -0500

    EncargadoCompras classes was added part1

---
+++ src/main/java/com/proyecto/baseDatos/consultas/DAOEncargadoComprasImpl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto.baseDatos.consultas;
+
+import com.proyecto.baseDatos.GestorBaseDatos;
+import com.proyecto.interfaces.DAOEncargadoComprasInterfaz;
+
+/**
+ *
+ * @author skiva
+ */
+public class DAOEncargadoComprasImpl extends GestorBaseDatos implements DAOEncargadoComprasInterfaz{
+    
+}
--- src/main/java/com/proyecto/entidades/EncargadoCompras.java
+++ src/main/java/com/proyecto/entidades/EncargadoCompras.java
@@ -8,6 +8,14 @@ package com.proyecto.entidades;
  *
  * @author skiva
  */
-public class EncargadoCompras {
-    
+public class EncargadoCompras extends Usuario{
+
+    public EncargadoCompras(int idUsuario, String nombre, String apellido_p, String apellido_m, String dni, String telefono, String username, String password, String rol) {
+        super(idUsuario, nombre, apellido_p, apellido_m, dni, telefono, username, password, rol);
+    }
+
+    @Override
+    public boolean autenticar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
